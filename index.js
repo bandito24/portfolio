c528cd38bcd5fa4adcaaa599103530aac6d79c92
Charlie Mudd <cmudd6@gmail.com>
2022-12-03 20:06:27 -0800

    js commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function change(){
+    document.getElementById("label-1").style.opacity= '0';
+
+}
+
+
+
+
+function change2(){
+    document.getElementById("label-2").style.opacity= '0'
+}
+
+
+
+
+
