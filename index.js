a83fab6f763eda3622c109958bc1b04388731a47
Charlie Mudd <cmudd6@gmail.com>
2022-12-03 22:38:56 -0800

    light & dark mode

--- index.js
+++ index.js
@@ -5,11 +5,33 @@ function change(){
 
 }
 
-
-
-
 function change2(){
-    document.getElementById("label-2").style.opacity= '0'
+    document.getElementById("label-2").style.opacity= '0';
+}
+
+let background = document.body;
+let aboutMe = document.getElementById('about-me');
+var node = document.getElementById('links').querySelector('img');
+
+
+function lightMode(){
+    background.style.backgroundImage='url(./images-2/white.jpg)'
+    background.style.color = 'hsla(0,0%,0%,.85)';
+    document.getElementById('about-me').style.border = "3px hsla(168, 76%, 36%, .75) solid";
+    aboutMe.querySelector('a').style.color = 'hsla(0,0%,0%,.85)';
+   
+    //document.getElementsByClassName('color-mode').style.boxShadow = "2px 2px 2px black";
+    document.getElementById('nav').querySelector('li').style.color = 'hsla(0,0%,0%,.85)'
+    node.style.backgroundColor = 'hsla(0,0%,50%,.5)'
+    node.style.borderRadius = '5px'
+ /*
+    for (let i = 0; i < node.clientHeight; i++){
+        node[i].style.backgroundColor = 'hsla(0,0%,50%,.5)';
+    };
+*/
+
+
+
 }
 
 
